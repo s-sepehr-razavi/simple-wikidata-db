f416392f93fe6fceacfaf5e7b1856177efd37348
s-sepehr-razavi <s.sepehr.razavi@gmail.com>
2025-07-14 11:29:16 +0330

    Adding semantic filtering (BERT and a test for LLM)

--- elastic/create_docred.py
+++ elastic/create_docred.py
@@ -1,15 +1,20 @@
 from elasticsearch import Elasticsearch
 from transformers import AutoTokenizer
-from collections import defaultdict
-from elasticsearch import Elasticsearch
 from collections import Counter, defaultdict
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from typing import List, Dict
+
 
 
 es = Elasticsearch("http://localhost:9200")
 
 # Load model once
+model = SentenceTransformer('m3hrdadfi/roberta-zwnj-wnli-mean-tokens')
 tokenizer = AutoTokenizer.from_pretrained("HooshvareLab/bert-fa-zwnj-base")
-property_set = {"P6": "head of government", "P17": "country", "P19": "place of birth", "P20": "place of death", "P22": "father", "P25": "mother", "P26": "spouse", "P27": "country of citizenship", "P30": "continent", "P31": "instance of", "P35": "head of state", "P36": "capital", "P37": "official language", "P39": "position held", "P40": "child", "P50": "author", "P54": "member of sports team", "P57": "director", "P58": "screenwriter", "P69": "educated at", "P86": "composer", "P102": "member of political party", "P108": "employer", "P112": "founded by", "P118": "league", "P123": "publisher", "P127": "owned by", "P131": "located in the administrative territorial entity", "P136": "genre", "P137": "operator", "P140": "religion", "P150": "contains administrative territorial entity", "P155": "follows", "P156": "followed by", "P159": "headquarters location", "P161": "cast member", "P162": "producer", "P166": "award received", "P170": "creator", "P171": "parent taxon", "P172": "ethnic group", "P175": "performer", "P176": "manufacturer", "P178": "developer", "P179": "series", "P190": "sister city", "P194": "legislative body", "P205": "basin country", "P206": "located in or next to body of water", "P241": "military branch", "P264": "record label", "P272": "production company", "P276": "location", "P279": "subclass of", "P355": "subsidiary", "P361": "part of", "P364": "original language of work", "P400": "platform", "P403": "mouth of the watercourse", "P449": "original network", "P463": "member of", "P488": "chairperson", "P495": "country of origin", "P527": "has part", "P551": "residence", "P569": "date of birth", "P570": "date of death", "P571": "inception", "P576": "dissolved, abolished or demolished", "P577": "publication date", "P580": "start time", "P582": "end time", "P585": "point in time", "P607": "conflict", "P674": "characters", "P676": "lyrics by", "P706": "located on terrain feature", "P710": "participant", "P737": "influenced by", "P740": "location of formation", "P749": "parent organization", "P800": "notable work", "P807": "separated from", "P840": "narrative location", "P937": "work location", "P1001": "applies to jurisdiction", "P1056": "product or material produced", "P1198": "unemployment rate", "P1336": "territory claimed by", "P1344": "participant of", "P1365": "replaces", "P1366": "replaced by", "P1376": "capital of", "P1412": "languages spoken, written or signed", "P1441": "present in work", "P3373": "sibling"}
+property_set = {'P6': 'رئیسجمهور یا شهردار', 'P17': 'کشور', 'P19': 'زادگاه', 'P20': 'محل مرگ', 'P22': 'پدرش', 'P25': 'مادرش', 'P26': 'همسر', 'P27': 'تبعۀ', 'P30': 'قاره', 'P31': 'نمونهای از', 'P35': 'حاکم', 'P36': 'پایتخت', 'P37': 'زبان رسمی', 'P39': 'منصب', 'P40': 'فرزند', 'P50': 'نویسنده', 'P54': 'عضو تیم ورزشی', 'P57': 'کارگردان', 'P58': 'فیلمنامهنویس', 'P69': 'دانشآموختهٔ', 'P86': 'آهنگساز', 'P102': 'عضو حزب', 'P108': 'کار میکند/میکرد در', 'P112': 'بنیانگذار', 'P118': 'لیگ', 'P123': 'ناشر', 'P127': 'دارنده', 'P131': 'موقعیت در تقسیمات کشوری', 'P136': 'سبک', 'P137': 'کارفرما', 'P140': 'دین', 'P150': 'زیربخش', 'P155': 'پس از', 'P156': 'پیش از', 'P159': 'محل ستاد', 'P161': 'با هنرمندی', 'P162': 'تهیهکننده', 'P166': 'جوایز', 'P170': 'پدیدآورنده', 'P171': 'آرایهٔ والد', 'P172': 'قوم', 'P175': 'اجرا کننده', 'P176': 'سازنده', 'P178': 'توسعهدهنده', 'P179': 'دنباله', 'P190': 'شهرهای خواهرخوانده', 'P194': 'مجلس', 'P205': 'کشورهای ساحلی دریاچه', 'P206': 'آب سطحی پیرامون', 'P241': 'شاخهٔ نظامی', 'P264': 'برچسب ضبط', 'P272': 'شرکتهای رسانه', 'P276': 'مکان کنونی', 'P279': 'زیرردۀ', 'P355': 'شرکتهای زیرمجموعه', 'P361': 'جزئی از', 'P364': 'زبان اصلی', 'P400': 'سکو', 'P403': 'میریزد به', 'P449': 'پخش شده از شبکه یا کانالِ', 'P463': 'از اعضای', 'P488': 'مدیر مسئول', 'P495': 'زادگاه فرهنگی', 'P527': 'دربرگیرنده', 'P551': 'جای زندگی', 'P569': 'زادروز', 'P570': 'درگذشت', 'P571': 'تاریخ ساخت\u202f/\u2009تأسیس', 'P576': 'تاریخ فروپاشی', 'P577': 'تاریخ انتشار', 'P580': 'زمان آغاز', 'P582': 'زمان پایان', 'P585': 'زمان رویداد', 'P607': 'جنگها', 'P674': 'شخصیتها', 'P676': 'سرایندۀ اشعار', 'P706': 'موقعیت', 'P710': 'با حضورِ', 'P737': 'متأثر از', 'P740': 'مکان شکلگیری', 'P749': 'نهاد ارشد', 'P800': 'اثر (ها)', 'P807': 'جداشده از', 'P840': 'مکان وقوع داستان', 'P937': 'محل انجام کار', 'P1001': 'متعلق به حوزهٔ قضایی', 'P1056': 'تولیدکنندهٔ', 'P1198': 'نرخ بیکاری', 'P1336': 'ادعا کننده مالکیت', 'P1344': 'شرکت داشته است در', 'P1365': 'به جای', 'P1366': 'جایگزینشده توسط', 'P1376': 'پایتخت است برای', 'P1412': 'زبانهای شخص', 'P1441': 'اشاره شده در کتابِ', 'P3373': 'همنیا'}
+property_description = {'P6': 'رئیس جمهور، استاندار، شهردار، دهدار', 'P17': 'سرزمین', 'P19': 'جایی که فرد متولد شده است (ریزترین جای در دسترس را بنویسید مثلا شهر به جای کشور)', 'P20': 'باید تا حد امکان دقیق و ریزتر باشد (مثلا شهر درگذشت نه کشور درگذشت)', 'P22': 'پدر این شخص', 'P25': 'مادر فرد مورد نظر', 'P26': 'همسر یا همبستر شخص موردنظر', 'P27': 'کشوری که شخص مورد نظر را به عنوان شهروند به رسمیت می\u200cشناسد', 'P30': 'قاره\u200cای که آیتم در آن است', 'P31': 'آیتم مورد نظر یک نوع ... است', 'P35': '', 'P36': 'مرکز حکومتی کشور یا ایالت یا سایر تقسیمات مدیریتی', 'P37': '', 'P39': 'منصب یا پست کنونی یا سابق آیتم', 'P40': 'فرزند(ان) شخص', 'P50': 'پدیدآورندهٔ اصلی یک اثر نوشتاری', 'P54': 'تیم یا باشگاه ورزشی که فرد مورد نظر در آن هست یا بوده است', 'P57': 'کارگردان فیلم، بازی ویدئویی یا موارد مشابه', 'P58': 'نویسندهٔ فیلم\u200cنامه', 'P69': 'آموزشگاهی که آیتم در آن تحصیل کرده است (نام دانشگاه یا مؤسسۀ آموزشی باید ذکر شود نه نام رشتۀ تحصیلی)', 'P86': 'شخصی که آهنگ را نوشته است', 'P102': 'حزب سیاسی که این سیاستمدار عضو آن است یا زمانی بوده است', 'P108': 'سازمانی که شخص برای آن کار می\u200cکند یا کار کرده است', 'P112': 'بنیان\u200cگذار یا بنیان\u200cگذاران مکان یا سازمان مورد نظر', 'P118': 'لیگی که تیم در آن بازی می\u200cکند', 'P123': 'سازمان انتشاراتی که اثر را منتشر کرده است', 'P127': 'صاحب حقوقی موضوع', 'P131': 'جایگاه آیتم مورد نظر در تقسیمات کشوری', 'P136': 'ژانر آثار خلاقه یا سبک کار یک هنرمند (فعلا برای موسیقی و موسیقی دانان به کار نبرید)', 'P137': '', 'P140': 'دین شخص (باید توسط خود وی یا منابع تاریخی گفته شده باشد)', 'P150': 'زیربخش مستقیم یک بخش اداری', 'P155': 'اثری که قبل از این اثر پدید آمده است.', 'P156': 'نام کار بعدی (در مجموعه های چندگانهٔ فیلم یا کتاب یا مجموعه تلویزیونی)', 'P159': 'محل دقیق دفتر مرکزی سازمان مورد نظر', 'P161': 'هنرمندانی که در یک فیلم نامشان با حروف درشت روی پوستر می آید (برای تعیین نقش اول یا نقش\u200cهای مرتبۀ پایین از گستره\u200cنمای P453 استفاده کنید)', 'P162': 'تهیه\u200cکنندهٔ این فیلم یا ترانه', 'P166': 'جوایزی که به فرد، سازمان، یا اثر داده شده است', 'P170': 'شخصی که اثر را پدید آورده است (هنگامی استفاده شود که خاصیت دقیق\u200cتری موجود نباشد)', 'P171': 'نزدیک\u200cترین آرایهٔ والد آرایهٔ مورد بحث', 'P172': 'قومیت آیتم', 'P175': 'بازیگر ، نوازنده ، گروه یا مجری دیگری که با این نقش یا اثر موسیقی مرتبط است', 'P176': 'سازندۀ اصلی(نیازی به آوردن نام سازندگان فرعی نیست)', 'P178': 'شخص یا شرکتی که این آیتم را توسعه داده است', 'P179': '', 'P190': '', 'P194': '', 'P205': 'کشوری که دریاچه در آن قرار دارد یا کشورهایی که در کنار یک دریاچه قرار دارند یا کشورهایی که منبع ورودی آب دریاچه یا مخزن خروجی آب دریاچه در آن ها قرار دارد', 'P206': 'دریاچه ای که مکان مورد نظر در آن واقع شده است', 'P241': '', 'P264': 'برچسب (آرم) نام تجاری که نوار یا سی دی یا ... با آن عنوان تکثیر می شود', 'P272': '', 'P276': 'مکان فعلی یک شیء در حال حرکت', 'P279': 'همهٔ این آیتم\u200cها زیرگروه آن  آیتم هستند؛ این آیتم بخشی از آن آیتم است.', 'P355': 'شرکت\u200cهای زیرمجموعۀ یک شرکت', 'P361': 'این آیتم جزئی است از آیتم ...', 'P364': 'اولین زبانی که فیلم، سی دی ویدئویی، ... مورد نظر (Deprecated for books, written texts, use P407 instead)به آن زبان ایجاد شده است (برای متون مانند کتاب\u200cها از P407 استفاده کنید)', 'P400': 'سکویی که یک اثر توسعه یا منتشر شده/سکوی نسخهٔ خاصی از یک نرم\u200cافزار توسعه\u200cیافته', 'P403': 'در زمینۀ هیدرولوژی', 'P449': 'شبکه\u200cای که اولین بار فیلم تلویزیونی موردنظر از آن پخش شده است', 'P463': 'سازمان یا باشگاهی(شامل گروه\u200cهای قومی یا اجتماعی نمی\u200cشود) که شخص مورد نظر به صورت داوطلبانه در آن عضو شده بود یا عضو آن هست', 'P488': 'میرمسئول یک سازمان یا رئیس یک گروه', 'P495': 'کشوری که این آیتم (اثر خلاقانه، خوراک، سخن، کالا و...) ریشه در آنجا دارد', 'P527': 'جزئی از', 'P551': 'جایی که شخص مورد نظر در آن زندگی می\u200cکرده یا زندگی می\u200cکند', 'P569': 'زمان به\u200cدنیا آمدن شخص مورد نظر', 'P570': 'زمان از دنیا رفتن شخص مورد نظر', 'P571': 'زمان راه\u200eاندازی سازمان یا بنای مورد نظر', 'P576': 'تاریخ فروپاشی یا ادغام سازمان مورد نظر', 'P577': 'زمانی که اثر فرهنگی مورد نظر منتشر شده است', 'P580': 'تاریخ به\u200eوجود\u200e آمدن آیتم مورد نظر یا دارای\u200eاعتبار شدن آن', 'P582': 'تاریخ  از بین رفتن آیتم مورد نظر یا از دست رفتن اعتبار ادعای مورد نظر', 'P585': 'زمان یا تاریخ وقوع یا زمان انتشار مطلب (در بخش منبع)', 'P607': 'جنگ\u200cهایی که شخص موردنظر در آن\u200cها جنیگیده است', 'P674': 'شخصیت\u200cهایی که در آیتم (نمایشنامه، اپرا، کتاب) مورد نظر حضور می\u200cیابند', 'P676': 'سرایندۀ اشعار ترانۀ مورد نظر', 'P706': 'موقعیت یک شی یا زمین\u200cچهره. برای تقسیمات کشوری از P131 استفاده شود', 'P710': 'افراد یا سازمان\u200cهای شرکت کننده در یک رویداد', 'P737': '', 'P740': 'مکانی که سازمان یا گروه مورد نظر در آنجا شکل گرفت', 'P749': '', 'P800': 'اثرهای قابل توجه علمی و هنری', 'P807': 'تأسیس یا شروع شده با جدا شدن از', 'P840': '', 'P937': 'محلی که شاغل در آنجا فعالیتش را انجام داده است', 'P1001': '', 'P1056': 'ماده\u200cای که معدن، مکان صنعتی یا فرآیند تولیدی مورد نظر آن را تولید می\u200cکند', 'P1198': 'بخشی از جمعیت دارای نیروی کار که فاقد کار هستند', 'P1336': 'کشوری که ادعای مالکیت بر منطقه مورد نظر را دارد', 'P1344': 'افراد سهیم در (بر عکس خصوصیت P107)', 'P1365': 'برای سازه جایگزین از P167 و از P155 زمانی که آیتم قبلی وجود دارد و قابل تشخیص است.', 'P1366': 'فرد دومی که جایگزین وی شده\u200cاست', 'P1376': '', 'P1412': 'زبانی که فرد با آن صحبت می\u200cکند یا اثری به آن زبان نوشته است', 'P1441': 'مثلاً به رستم در کتاب شاهنامه اشاره شده است', 'P3373': ''}
+
 
 def search_by_property(index: str, field: str, value: str, use_keyword: bool = False):
     """
@@ -47,6 +52,57 @@ def search_by_property(index: str, field: str, value: str, use_keyword: bool = F
     return result
 
 
+def get_triples_score(text, triples, model):
+    sentences = []
+    for triple in triples:        
+        sentences.append(f"{triple['head']} {property_set[triple['relation']]} ({property_description[triple['relation']]}) {triple['tail']}")
+
+    sentences_1 = [text]
+
+    embeddings_1 = model.encode(sentences_1,                             
+                                max_length=1000, # If you don't need such a long length, you can set a smaller value to speed up the encoding process.
+                                )
+    embeddings_2 = model.encode(sentences)
+    similarity = embeddings_1 @ embeddings_2.T
+    
+    for i in range(len(triples)):
+        triples[i]['score'] = similarity[0][i]
+
+
+def filter_by_score(data: List[Dict], node) -> List[Dict]:
+    """
+    Groups the input list of dictionaries by 'head', then by 'head_id',
+    and retains only the subgroup (per head) with the highest cumulative score.
+
+    Args:
+        data (List[Dict]): List of dictionaries with 'head', 'head_id', and 'score'.
+
+    Returns:
+        List[Dict]: Filtered list with only the highest-scoring subgroup per 'head'.
+    """
+    result = []
+    groups = defaultdict(list)
+
+    # Group by 'head'
+    for item in data:
+        groups[item[node]].append(item)
+
+    # Within each head group, group by 'head_id' and select highest scoring group
+    for _, items in groups.items():
+        id_groups = defaultdict(list)
+        for item in items:
+            id_groups[item[f'{node}_id']].append(item)
+
+        # Select the head_id group with the maximum total score
+        max_id = max(
+            id_groups.items(),
+            key=lambda x: sum(i['score'] for i in x[1])
+        )[0]
+
+        result.extend(id_groups[max_id])
+
+    return result
+
 def resolve_entity_qids(data):
     # Count frequencies of (name, qid) pairs for both head and tail
     name_qid_counter = Counter()
@@ -74,7 +130,6 @@ def resolve_entity_qids(data):
             filtered_data.append(entry)
 
     return filtered_data
-
 
 
 def find_all_phrase_token_spans(text, phrases):    
@@ -111,23 +166,30 @@ def find_all_phrase_token_spans(text, phrases):
             # Move search start past this occurrence
             search_start = phrase_start + 1
 
-        results[phrase] = phrase_spans if phrase_spans else None
+        results[phrase] = phrase_spans if phrase_spans else [] #BUG
 
     return tokens, results
 
 
-def process_doc(doc):
-    
+def process_doc(doc):    
     doc_id = doc['_id']
+    # print(doc_id)
     triples = search_by_property("triples", "doc_id", doc_id, use_keyword=False)
-    triples = resolve_entity_qids(triples)
+    # print(f"before filtering by score: {len(triples)}")
+    # triples = resolve_entity_qids(triples)
+    print(f"before filtering with score triples: {len(triples)}")
+    get_triples_score(doc['_source']['abstract'], triples, model)
+    triples = filter_by_score(triples, 'head')
+    triples = filter_by_score(triples, 'tail')
+    print(f"after filtering with score triples: {len(triples)}")
+    # print(f"after filtering by score: {len(triples)}")
 
     entity_dict = {}
     relations = []
     for triple in triples:
         entity_dict[triple['head']] = triple['head_id']
         entity_dict[triple['tail']] = triple['tail_id']
-        relations.append((triple['head_id'], triple['tail_id'], triple['relation']))
+        relations.append((triple['head_id'], triple['tail_id'], triple['relation'], triple['score']))
     
     tokens, results = find_all_phrase_token_spans(doc['_source']['abstract'], entity_dict.keys())
 
@@ -135,10 +197,13 @@ def process_doc(doc):
     for alias in results.keys():
         qid = entity_dict[alias]
         if qid in qid_occurence.keys():
-            qid_occurence[qid] += results[alias]
+            try:
+                qid_occurence[qid] += results[alias]
+            except:
+                print(results)
+                return
         else:
             qid_occurence[qid] = results[alias]
-    
     return {
         'title': doc['_source']['title'],
         'tokens':tokens,
@@ -150,9 +215,10 @@ def process_doc(doc):
 
 def process_batch(batch):
     for doc in batch:
+        print(doc['_id'])
         abstract = doc["_source"]['abstract']
         if len(abstract.split()) > 128:
-            result = process_doc(doc)
+            result = process_doc(doc)            
             print(result)
         print("="*30)
 
